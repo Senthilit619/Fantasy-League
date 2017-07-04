da691266475b2d6c63ab3b82d8e3e9ed030a0f21
Pradeep Kumar R <pradeepraghu9427@gmail.com>
2017-07-05 01:01:44 +0530

    Home page offcanvas creation

---
+++ JS/home.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.location-btn').click(function(){
+      $('#location-canvas').toggleClass('display-location');
+  });
+  $('#user-menu-button').click(function(){
+      $('#user-menu-canvas').toggleClass('display-usermenu');
+  });
+});
