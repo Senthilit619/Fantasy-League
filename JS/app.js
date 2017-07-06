a27989d824f0428ba890b4788ef2c08e8014cfa4
Senthilit619 <krsenthilit619@gmail.com>
2017-07-07 04:24:42 +0530

    Components for signup

--- JS/app.js
+++ JS/app.js
@@ -1,30 +1,103 @@
 var app=angular.module('app',[]);
 
-app.controller('Login-Controller',function($scope,$rootScope){
+app.controller('Login-Controller',function($scope,$rootScope,$http){
   $scope.kickoffLogin=true;
   $scope.kickoffSignup=null;
   $scope.strength=null;
   $scope.flag1=false;
   $scope.flag2=false;
+  $scope.confirm_message=null;
+  $scope.validate_message=null;
+  $scope.availability_message=null;
+  $rootScope.availability=null;
+  $rootScope.not_availability=null;
+  $rootScope.loading=null;
 
-  $scope.Signup=function(){
+  $scope.display = function(name){
+    console.log(name);
+  }
+  $scope.checkAvailability = function(name,form){
+    console.log("Checking availability");
+    $rootScope.loading=true;
+    $http.get("checkavailability", {params:{"name": name}}).then(function(response){
+          $rootScope.loading=false;
+          console.log(form.username.$dirty);
+          if(name == undefined)
+          {
+            $rootScope.availability = false;
+            $rootScope.not_availability = false;
+          }
+
+          if(response.data.length == 0 && name != undefined && form.username.$dirty==true){
+              $scope.availability_message="Username Available";
+              $rootScope.availability=true;
+              $rootScope.not_availability=false;
+          }
+          else if(response.data.length != 0 && name != undefined && form.username.$dirty==true){
+            $scope.availability_message="Sorry! Username already taken";
+            $rootScope.availability=false;
+            $rootScope.not_availability=true;
+          }
+          console.log($rootScope.availability);
+      },function(err){
+          console.log(err);
+        });
+  }
+
+  $scope.Signup=function(form){
     if ($scope.kickoffLogin===true){
       $scope.kickoffLogin=false;
       $scope.kickoffSignup=true;
-      console.log(1);
+      angular.element(document.body).append(form);
     }else {
       $scope.kickoffLogin=true;
       $scope.kickoffSignup=false;
-      console.log(2);
     }
   }
 
-  $scope.validate=function(user){
+  $scope.passwordValidation = function(pwrd){
+    var patt1 = /\d/; //pattern to match the digit 
+    var patt2 = /\D/; //pattern to match the non-digit
+    var split_pwrd = pwrd.slice(1,pwrd.length);
+    var result1 = patt1.test(split_pwrd); //values for comparison
+    var result2 = patt2.test(split_pwrd);
+    if(pwrd[0]!=pwrd[0].toUpperCase())
+        $scope.validate_message="First letter must be Capital letter";
+    else{
+        $scope.validate_message=false;
+      if(result2==false)
+        $scope.validate_message="Password must contain lowercase alphabets";
+      else{
+        $scope.validate_message=false;
+        if(result1==false)
+            $scope.validate_message="Password must contain numbers";
+        else{
+          $scope.validate_message=false;
+          if(pwrd.length<8)
+            $scope.validate_message="Password must be atleast 8 characters long";
+          else
+              $scope.validate_message=false;
+        }
+      }
+    }
+  }
+
+ $scope.passwordConfirmation=function(pwrd,con_pwrd,form){
+    if(pwrd!=con_pwrd)
+      $scope.confirm_message=true;
+    else{
+      $scope.confirm_message=false;
+    }
+  }
+  $scope.validateForm = function(form){
+    if($scope.confirm_message==true && $scope.validate_message==false)
+        form.$setValidity('form',false);
+    else
+      form.$setValidity('form',true);
   }
 
   //Function to validate the strength and pattern of the password entered by the user while signing up 
   $scope.passwordStrengthValidation= function(password){
-    console.log(password);
     var patt1 = /\d/; //pattern to match the digit 
     var patt2 = /\D/; //pattern to match the non-digit
     var patt3 = /^([A-Z][a-z0-9]{1,})$/; //pattern to check capital letter at first and alphanumeric for rest
@@ -33,21 +106,17 @@ app.controller('Login-Controller',function($scope,$rootScope){
     var result2 = patt2.test(password);
     if (password!=undefined && password!=''){
         if (result1!=false && $scope.flag1==false){
-          console.log('inside result1');
           $scope.strength+=33;
           $scope.flag1=true;
         }
         if(result1==false && $scope.flag1==true){
-          console.log('inside result1 strength');
           $scope.strength-=33;
         }
         if (result2!=false && $scope.flag2==false){
-          console.log('inside result2');
           $scope.strength+=33;
           $scope.flag2=true;
         }
         if(result2==false && $scope.flag2==true){
-          console.log('inside result2 strength');
           $scope.strength-=33;
         }
         if(password.match(patt1)==null){
@@ -61,16 +130,14 @@ app.controller('Login-Controller',function($scope,$rootScope){
             $scope.strength+=34;
           }
         }
-        console.log($scope.strength);
-    }else{
+      }
+      else{
       $scope.strength=0;
       result1=false;
       result2=false;
       $scope.flag1=false;
       $scope.flag2=false;
     }
-    
-     
   }
 });
 
--- JS/mongo.js
+++ JS/mongo.js
@@ -10,6 +10,7 @@ MongoClient.connect("mongodb://senthilit:rafanadal619@ds123312.mlab.com:23312/fo
   }
 });
 
+//Login User 
 var exports=module.exports;
 exports.authenticateUser = function(l_name,l_password){
 	console.log("Authenticating User");
@@ -22,4 +23,38 @@ exports.authenticateUser = function(l_name,l_password){
   			console.log(data);
   		}
   	});
+}
+//Signup User -Add new User
+exports.signupUser = function(s_name,s_password,s_email,s_dob){
+  console.log("Adding new User");
+    mongoobj.db.collection('UserDetails').insert({"name": s_name,"password":s_password,"email":s_email,"dob":s_dob},function(err,data){
+      if(err){
+        console.log("Error signing up. Try Again");
+      }
+      else{
+        console.log("Successfully Signed Up");
+        console.log(data);
+      }
+    });
+}
+
+//Check Availability of the User
+exports.checkUsername = function(uname,callback){
+  result=null;
+  console.log("Checking Username Availability:"+uname);
+    mongoobj.db.collection('UserDetails').find({'name': uname}).toArray(function(err,data){
+      if(err){
+        console.log("Try Again");
+        console.log(err);
+      }
+      else{
+        console.log("Username Available. Try Another Name");
+        if(data!=''){
+        console.log("Data:"+data);
+          callback(data);
+        }
+        else
+          callback(data);
+      }
+    });
 }
